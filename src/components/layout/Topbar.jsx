4e81bbec3d0384865737fd4767339626d6c45206
Danish-Razzaq <danishrazzaq371@gmail.com>
2025-06-05 13:58:31 +0500

    UI isue resolve

--- src/components/layout/Topbar.jsx
+++ src/components/layout/Topbar.jsx
@@ -71,14 +71,14 @@ const Topbar = ({ toggleSidebar, open }) => {
           </div>
 
           {/* Center: Search */}
-          <div className="flex-1 px-8 relative">
+          <div className="flex-1 px-8 relative max-md:hidden">
             <div className="relative w-fit group transition-all duration-300">
               <Search className="absolute left-3 top-2.5 text-primary pointer-events-none" />
 
               <input
                 type="text"
                 placeholder="Search..."
-                className="pl-10 pr-4 py-2 text-black rounded-[3px] border border-gray-300 focus:outline-none focus:ring-2 focus:ring-primary transition-all duration-300  w-64 max-md:hidden group-focus-within:w-72"
+                className="pl-10 pr-4 py-2 text-black rounded-[3px] border border-gray-300 focus:outline-none focus:ring-2 focus:ring-primary transition-all duration-300  w-64  group-focus-within:w-72"
               />
             </div>
           </div>
